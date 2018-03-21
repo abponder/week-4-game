07f06a67e0341d72fea69c0700f725361290ae6d
alexbponder@gmail.com <alexbponder@gmail.com>
2018-03-20 23:14:33 -0700

    add final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,78 +3,116 @@
 // List of Words only
 var wins = 0;
 var losses = 0;
-var ResetStartGuesses = 20;
-var CountOfMissedGuesses = 0;
-var RemainingGuesses = 20;
-var misses = 0;
-var validGuess = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var wordLetterCount = 0;
-var WordAnswerDashes = [];
+var score = 0;
+var scoreDisplay = 0;
+var crystalValue = 0;
+//var targNum = 55;
+var crystalRed = 0;
+var crystalBlue = 0;
+var crystalYellow = 0;
+var crystalGreen = 0;
 
-var words = ["Kimchi","Bibimbap","Bulgogo","Japchae","Hoeddeok",
-                "Ddukbokkie","Samgyeopsal","Chimaek","Tteokbokki"]
-
-// List of Words 
-    var WordListDetl = {
-        "Kimchi":"Fermented Vegetable",
-        "Bibimbap":"Mixed Rice",
-        "Bulgogi":"Marinated Beef Barbecue",  
-        "Japchae":"Stir Fried Noodles",
-        "Hoeddeok":"Sweet Syrupy Pancakes",
-        "Ddukbokkie":"Spicy Rice Cake",
-        "Samgyeopsal":"Noodles and Pork",
-        "Chimaek":"Fried Chicken and Beer",
-        "Tteokbokki":"Nobody knows don't ask"
-      };
+var crystalMin = 1;
+var crystalMax = 12;
+var targetMax = 120
+var targetMin = 19
+var lockGame = false;
 
 
-// Determine which key was pressed, make it lowercase, and set it to the userInput variable.
-//       var userInput = String.fromCharCode(event.which).toLowerCase();
+    // Initial Variables
+    var targNum = Math.floor((Math.random() * (targetMax - targetMin +1 )) + targetMin);
+    document.getElementById("targNum").innerHTML = targNum;
+    // set red to a random number
+    crystalRed = Math.floor((Math.random() * crystalMax) + crystalMin);
+    document.getElementById("cryred").innerHTML = crystalRed;
+    // set red to a random number
+    crystalBlue = Math.floor((Math.random() * crystalMax) + crystalMin);
+    document.getElementById("cryblu").innerHTML = crystalBlue;
+    // set red to a random number
+    crystalYellow = Math.floor((Math.random() * crystalMax) + crystalMin);
+    document.getElementById("cryyel").innerHTML = crystalYellow;
+    // set red to a random number
+    crystalGreen = Math.floor((Math.random() * crystalMax) + crystalMin);
+    document.getElementById("crygrn").innerHTML = crystalGreen;
 
-// This function is run whenever the user presses the first key.
-    document.onkeyup = function(event) {
 
-    // Determines which key was pressed.
-        var userGuess = event.key;
+   
+  // Here we created an on-click event that responds to button clicks of the crystal image.
+  $(".thumbnail a").on("click", ".crystal-image", function() {
+ 
+            console.log(crystalRed)
+            console.log($(this).attr("alt"))
+            console.log(crystalBlue)
+            console.log($(this).attr("alt"))
+            console.log(crystalYellow)
+            console.log($(this).attr("alt"))
+            console.log(crystalGreen)
+            console.log($(this).attr("alt"))
 
-    // Generate random number limited by word array length
-        var randNum = Math.floor(Math.random() * words.length); // not with +1 like: var randNum = Math.floor(Math.random() * words.length) + 1;
+            var crystalValue = ($(this).attr("data-crystalvalue")); //data-crystalvalue
+            crystalValue = parseInt(crystalValue);
+            // We then add the crystalValue to the user's "counter" which is a global variable.
+            // Every click, from every crystal adds to the global counter.
+            score += crystalValue;
+    if (lockGame !== true) {
+     // Update score
+     document.getElementById("score").innerHTML = score;
 
-    // Use random number as item number in word array to select the actual word
-        var computerGuess = words[randNum];
-
-    // Set up the answer array
-    var WordAnswerDashes = [];
-    for (var i = 0; i < computerGuess.length; i++) {
-        WordAnswerDashes[i] = "_";
+     // With each click the counter will increase by 10 and be re-evaluated against target.
+     if (score === targNum) {
+     // If the numbers match we'll celebrate the user's win.
+     document.getElementById("winLose").innerHTML = "You Win!!! You hit the target number of: " + targNum;
+     score = 0;
+     document.getElementById("score").innerHTML = score;
+     wins = 1;
+     document.getElementById("wins").innerHTML = wins;
+     lockGame = true;
+      }
+    // Here we added an "else if" condition. If the user's counter ever exceeds the targetNumber...
+     else if (score >= targNum) {
+        document.getElementById("winLose").innerHTML = "You Lose!!! You're over the target number of: " + targNum;
+        score = 0;
+        document.getElementById("score").innerHTML = score;
+        losses = 1;
+        document.getElementById("losses").innerHTML = losses;
+        lockGame = true;
+        }
     }
-    var LettersToGuess = computerGuess.length;
 
-        console.log(LettersToGuess);
-        console.log(WordAnswerDashes);
 
-    // send output to html id tags NOT PART OF FINAL VERSION!
-    document.getElementById("demo0").innerHTML = userGuess;
-    document.getElementById("demo1").innerHTML = randNum;
-    document.getElementById("demo2").innerHTML = computerGuess;
+    // // Run game
+    //   reset();
+  });
 
-    var html =
-    "<hr>" +
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<hr>" +
-    "<p>You chose: " + userGuess + "</p>" +
-    "<hr>" +
-    "<p>The computer chose: </p>" +
-    "<p>Take a guess >>: " + WordAnswerDashes + "</p>" +
-    "<hr>" +
-    "<p>misses: " + misses + "</p>";
+
+ 
+
+
+
+
+
+
+
+// Generate random number limited by array length or max number:
+    //var targetNum = Math.floor((Math.random() * targetMax) + targetMin);
+    //document.getElementById("targNum").innerHTML = targetNum;
+
+
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("score").innerHTML = score;
+
+
+
+
+
+
+
+
+
+
+
   
-          // Set the inner HTML contents of the #game div to our html string
-          document.querySelector("#game").innerHTML = html;
-
-  
-};
 
   
 // Start Clock Function
